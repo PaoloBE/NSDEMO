45333cd6c4ab8da5851e2700616fdbd79e1ec170
PAOLO DANIEL BUDIEL ECHEVARRIA <budiel420@hotmail.com>
2020-03-03 16:39:22 -0500

    'assignment1'

---
+++ app/shared/drawer/drawer.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'drawer-content',
+    templateUrl: './drawer.component.html',
+})
+export class DrawerComponent {
+
+    constructor() {
+    }
+}
