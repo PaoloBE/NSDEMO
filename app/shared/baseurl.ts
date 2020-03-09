3a7e612fefcefc74e64d8a4472d4735b89663cdb
unknown <budiel420@hotmail.com>
2020-03-08 23:52:55 -0500

    'waa'

--- app/shared/baseurl.ts
+++ app/shared/baseurl.ts
@@ -1,2 +1,2 @@
-//export const baseURL = "http://192.168.1.4:3000/";
-export const baseURL = "http://172.25.18.70:3000/";
+export const baseURL = "http://192.168.1.4:3000/";
+//export const baseURL = "http://172.25.18.70:3000/";
